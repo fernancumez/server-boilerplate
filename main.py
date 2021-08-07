394d02c95fa73e57b79795e0b6af9d26b1b8e3c7
fernancumez <fernancumez@gmail.com>
2021-08-06 23:11:08 -0600

    deployment to heroku

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
 from flask import Flask, render_template, request, url_for, redirect
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html')
 
@@ -22,4 +23,5 @@ def contact():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port)
