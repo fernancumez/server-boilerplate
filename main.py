27f4580376f478045020ccba7e03630420dfbc9d
fernancumez <fernancumez@gmail.com>
2021-08-06 21:33:51 -0600

    form validations added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -6,5 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/contact", methods=["POST"])
+def contact():
+    payload = {
+      "name": request.form["name"],
+      "email": request.form["email"],
+      "description": request.form["description"],
+    }
+
+    file = open('data.txt', 'w')
+    file.write(payload.__str__())
+    file.close()
+
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
